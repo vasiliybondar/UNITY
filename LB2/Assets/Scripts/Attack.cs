c4d4de708595405723b9ae4db5cc4b78b0222e35
Vasiliy Bondar <vasiliy.b99@icoud.com>
2019-12-16 09:55:23 +0200

    Added class Attack, change class NPC

---
+++ LB2/Assets/Scripts/Attack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LB2
+{
+    class Attack : Interacteble
+    {
+        private bool charAttack;
+        private bool enemyAttack;
+
+        private void Awake()
+        {
+            charAttack = true;
+            enemyAttack = false;
+        }
+
+        private void Update()
+        {
+            // check animation
+            // if (animation == State.Attck && animatorEnemy == stateAttack)
+            // Attack()
+        }
+
+        protected override void Interact()
+        {
+            //Взаємодія
+        }
+
+        private void CheckAttack()
+        {
+            // check attack radius
+            // {
+            //  if( charAttack == true)
+            //      GetDamage() - enemy
+            //  else
+            //      base.GetDamage()
+            // }
+            ChangeAttack();
+        }
+
+        private void ChangeAttack()
+        {
+            charAttack = !charAttack;
+            enemyAttack = !enemyAttack;
+        }
+    }
+}
--- LB2/Assets/Scripts/NPC.cs
+++ LB2/Assets/Scripts/NPC.cs
@@ -9,43 +9,22 @@ namespace LB2
 {
     class NPC : Interacteble
     {
-        private bool charAttack;
-        private bool enemyAttack;
-
-        private void Awake()
-        {
-            charAttack = true;
-            enemyAttack = false;
-        }
+        private string nameNPC;
 
         private void Update()
         {
-            // check animation
-            // if (animation == State.Attck && animatorEnemy == stateAttack)
-            // Attack()
+            //if GetButtonUp()
+            //Quest()
+        }
+
+        private void Quest()
+        {
+            //Взаємодія
         }
 
         protected override void Interact()
         {
             //Взаємодія
-        }
-
-        private void Attack()
-        {
-            // check attack radius
-            // {
-            //  if( charAttack == true)
-            //      GetDamage() - enemy
-            //  else
-            //      base.GetDamage()
-            // }
-            ChangeAttack();
-        }
-
-        private void ChangeAttack()
-        {
-            charAttack = !charAttack;
-            enemyAttack = !enemyAttack;
         }
     }
 }
